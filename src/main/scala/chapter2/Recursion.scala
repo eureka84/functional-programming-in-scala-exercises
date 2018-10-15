70e24a891284cbe587a55e38dc5111db843797ab
eureka84 <angelosciarra@ymail.com>
2018-10-15 19:12:58 +0200

    tailrec annotation (to signal at compile time if our recursion is not tail recursive)

--- src/main/scala/chapter2/Recursion.scala
+++ src/main/scala/chapter2/Recursion.scala
@@ -1,11 +1,14 @@
 package chapter2
 
+import scala.annotation.tailrec
+
 /**
   * @author asciarra 
   */
 object Recursion {
 
   def fib(n: Int): Int = {
+    @tailrec
     def loop(counter: Int, last: Int, secondToLast: Int): Int =
       if (counter == n) last
       else loop(counter + 1, last + secondToLast, last)
