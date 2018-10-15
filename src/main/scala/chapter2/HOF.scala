9cefd18c2e78974649fdfc625c74c1a12db1cf67
eureka84 <angelosciarra@ymail.com>
2018-10-15 19:58:07 +0200

    minor change

--- src/main/scala/chapter2/HOF.scala
+++ src/main/scala/chapter2/HOF.scala
@@ -8,9 +8,7 @@ object HOF {
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean =
     if (as.length < 2) true
     else
-      (0 until as.length - 1)
-        .zip(1 until as.length)
+      (0 until as.length - 1).zip(1 until as.length)
         .forall(pair => ordered(as(pair._1), as(pair._2)))
-
 
 }
