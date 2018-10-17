d421751722b7107a3d48c21fcb4028f4acbe69f6
asciarra <angelo.sciarra@lastminute.com>
2018-10-17 16:19:49 +0200

    minor: reformat

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -1,7 +1,9 @@
 package chapter3
 
 sealed trait List[+A]
+
 case object Nil extends List[Nothing]
+
 case class Cons[A](head: A, tail: List[A]) extends List[A]
 
 object List {
@@ -22,7 +24,7 @@ object List {
 
   def drop[A](l: List[A], n: Int): List[A] =
     if (n == 0) l
-    else drop(tail(l), n-1)
+    else drop(tail(l), n - 1)
 
   def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
     case Cons(h, t) => if (f(h)) dropWhile(t)(f) else l
@@ -36,28 +38,30 @@ object List {
     case Cons(h, t) => Cons(h, init(t))
   }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
     as match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
-//  def length[A](as: List[A]): Int = foldRight(as, 0)((_, acc) => acc + 1)
+  //  def length[A](as: List[A]): Int = foldRight(as, 0)((_, acc) => acc + 1)
   def length[A](as: List[A]): Int = foldLeft(as, 0)((acc, _) => acc + 1)
 
-  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
     as match {
       case Nil => z
       case Cons(h, t) => foldLeft(t, f(z, h))(f)
     }
 
-    def reverse[A](xs: List[A]): List[A] = foldLeft(xs, Nil:List[A])((tail, head) =>  Cons(head, tail))
+  def reverse[A](xs: List[A]): List[A] = foldLeft(xs, Nil: List[A])((tail, head) => Cons(head, tail))
 
 }
 
 object IntList {
+
   import List._
 
-  def sum(xs: List[Int]): Int = foldLeft(xs, 0)(_+_)
-  def product(xs: List[Int]): Int = foldLeft(xs, 1)(_*_)
+  def sum(xs: List[Int]): Int = foldLeft(xs, 0)(_ + _)
+
+  def product(xs: List[Int]): Int = foldLeft(xs, 1)(_ * _)
 }
