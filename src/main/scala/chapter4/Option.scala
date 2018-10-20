bb5f13592d6d9f591895aacb841b2262b7b78527
eureka84 <angelosciarra@ymail.com>
2018-10-20 21:11:24 +0200

    implemented variance

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -35,3 +35,17 @@ sealed trait Option[+A] {
 case class Some[+A](v: A) extends Option[A]
 case object None extends Option[Nothing]
 
+object Math {
+
+  def mean(xs:Seq[Double]): Option[Double] = xs match {
+    case Nil => None
+    case _ => Some(xs.sum/xs.length)
+  }
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs)
+      .flatMap(m =>
+        mean(xs.map(x => math.pow(x - m, 2)))
+      )
+
+}
--- src/test/scala/chapter4/OptionTest.scala
+++ src/test/scala/chapter4/OptionTest.scala
@@ -5,42 +5,57 @@ import org.scalatest._
 class OptionTest extends FunSuite with Matchers {
 
   test("map") {
-    assert(Some(1).map(_.toString) == Some("1"))
-    assert(None.map(_.toString) == None)
+    Some(1).map(_.toString) shouldEqual Some("1")
+    None.map(_.toString) shouldEqual None
   }
 
   test("flatMap") {
-    def inverse(x: Int): Option[Double] = if (x == 0) None else Some(1.0/x)
-    assert(Some(1).flatMap(inverse) == Some(1.0))
-    assert(Some(0).flatMap(inverse) == None)
+    def inverse(x: Int): Option[Double] = if (x == 0) None else Some(1.0 / x)
+
+    Some(1).flatMap(inverse) shouldEqual Some(1.0)
+    Some(0).flatMap(inverse) shouldEqual None
   }
 
-  test("getOrElse on Some"){
+  test("getOrElse on Some") {
     Some(2).getOrElse(0) shouldEqual 2
   }
 
-  test("getOrElse on None"){
+  test("getOrElse on None") {
     None.getOrElse(0) shouldEqual 0
   }
 
-  test("orElse on Some"){
+  test("orElse on Some") {
     Some(2).orElse(Some(0)) shouldEqual Some(2)
   }
 
-  test("orElse on None"){
+  test("orElse on None") {
     None.orElse(Some(0)) shouldEqual Some(0)
   }
 
-  test("filter on Some true"){
-    Some(2).filter(_%2==0) shouldEqual Some(2)
+  test("filter on Some true") {
+    Some(2).filter(_ % 2 == 0) shouldEqual Some(2)
   }
 
   test("filter on Some false") {
     Some(2).filter(_ % 3 == 0) shouldEqual None
   }
 
-  test("filter on None"){
+  test("filter on None") {
     None.orElse(Some(0)) shouldEqual Some(0)
+  }
+
+  test("mean") {
+    import Math._
+
+    mean(Nil) shouldEqual None
+    mean(List(1, 2, 3)) shouldEqual Some(2)
+  }
+
+  test("variance") {
+    import Math._
+
+    variance(List(1, 4, 7)) shouldEqual Some(6.0)
+    variance(Nil) shouldEqual None
   }
 
 }
