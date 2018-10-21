01a0f6545fab4cf817d3c1ed79d883cbc45ff72c
eureka84 <angelosciarra@ymail.com>
2018-10-21 11:48:51 +0200

    rewrote sequence in terms of traverse

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -51,10 +51,13 @@ object Option {
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a flatMap (valueA => b map (valueB => f(valueA, valueB)))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
-    case Nil => Some(Nil)
-    case hOpt :: tOpts => hOpt flatMap (h => sequence(tOpts) map (t => h :: t))
-  }
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(identity)
+
+//  a match {
+//    case Nil => Some(Nil)
+//    case hOpt :: tOpts => hOpt flatMap (h => sequence(tOpts) map (t => h :: t))
+//  }
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
     case Nil => Some(Nil)
