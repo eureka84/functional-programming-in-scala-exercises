3a152681f08c475a9d878954bfff3f190d843088
eureka84 <angelosciarra@ymail.com>
2018-10-31 21:58:25 +0100

    rewrote candy dispenser in terms of State functions

--- src/main/scala/chapter6/CandyDispenser.scala
+++ src/main/scala/chapter6/CandyDispenser.scala
@@ -1,5 +1,7 @@
 package chapter6
 
+import State._
+
 sealed trait Input
 
 case object Coin extends Input
@@ -10,17 +12,36 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object Machine {
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
-    State((m: Machine) => {
-      val machine = inputs.foldLeft(m)(handleInput)
-      ((machine.coins, machine.candies), machine)
-    })
+  private def update: Input => Machine => Machine = (i: Input) => (s: Machine) =>
+    (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(locked = false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(locked = true, candy - 1, coin)
+    }
 
-  private def handleInput(m: Machine, i: Input): Machine = i match {
-    case _ if m.candies == 0 => m
-    case Coin if m.locked && m.candies > 0 => m.copy(locked = false, coins = m.coins + 1)
-    case Turn if !m.locked => m.copy(locked = true, candies = m.candies - 1)
-    case _ => m
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    val fromInputToMachineState = update andThen modify[Machine]
+    for {
+      _ <- sequence(inputs map fromInputToMachineState)
+      s <- get
+    } yield (s.coins, s.candies)
   }
+
+//  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+//    State((m: Machine) => {
+//      val machine = inputs.foldLeft(m)(handleInput)
+//      ((machine.coins, machine.candies), machine)
+//    })
+//
+//  private def handleInput(m: Machine, i: Input): Machine = i match {
+//    case _ if m.candies == 0 => m
+//    case Coin if m.locked => m.copy(locked = false, coins = m.coins + 1)
+//    case Turn if !m.locked => m.copy(locked = true, candies = m.candies - 1)
+//    case _ => m
+//  }
 
 }
--- src/main/scala/chapter6/State.scala
+++ src/main/scala/chapter6/State.scala
@@ -3,8 +3,8 @@ package chapter6
 import State._
 
 case class State[S, +A](run: S => (A, S)) {
-  def flatMap[B](g: A => State[S, B]): State[S, B] =
-    State((s: S) => {
+
+  def flatMap[B](g: A => State[S, B]): State[S, B] = State(s => {
       val (a, nextState) = run(s)
       g(a).run(nextState)
     })
@@ -16,6 +16,14 @@ case class State[S, +A](run: S => (A, S)) {
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     flatMap(a => sb.map(b => f(a, b)))
 
+}
+
+object State {
+  def unit[S, A](a: A): State[S, A] = State((s: S) => (a, s))
+
+  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
+    fs.foldRight(unit[S, List[A]](List[A]()))((sa, acc) => sa.map2(acc)(_ :: _))
+
   def modify[S](f: S => S): State[S, Unit] = for {
     s <- get
     _ <- set(f(s))
@@ -24,11 +32,4 @@ case class State[S, +A](run: S => (A, S)) {
   def get[S]: State[S, S] = State(s => (s, s))
 
   def set[S](s: S): State[S, Unit] = State(_ => ((), s))
-}
-
-object State {
-  def unit[S, A](a: A): State[S, A] = State((s: S) => (a, s))
-
-  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
-    fs.foldRight(unit[S, List[A]](List[A]()))((sa, acc) => sa.map2(acc)(_ :: _))
 }
