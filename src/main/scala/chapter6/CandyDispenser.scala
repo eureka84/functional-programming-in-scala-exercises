4663ee6b3a63b9342b732492442951826c81d856
eureka84 <angelosciarra@ymail.com>
2018-11-01 10:36:12 +0100

    minor change

--- src/main/scala/chapter6/CandyDispenser.scala
+++ src/main/scala/chapter6/CandyDispenser.scala
@@ -17,16 +17,14 @@ object Machine {
       case (_, Machine(_, 0, _)) => s
       case (Coin, Machine(false, _, _)) => s
       case (Turn, Machine(true, _, _)) => s
-      case (Coin, Machine(true, candy, coin)) =>
-        Machine(locked = false, candy, coin + 1)
-      case (Turn, Machine(false, candy, coin)) =>
-        Machine(locked = true, candy - 1, coin)
+      case (Coin, Machine(true, candy, coin)) => Machine(locked = false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) => Machine(locked = true, candy - 1, coin)
     }
 
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
-    val fromInputToMachineState = update andThen modify[Machine]
+    val toStates: Input => State[Machine, Unit] = update andThen modify[Machine]
     for {
-      _ <- sequence(inputs map fromInputToMachineState)
+      _ <- sequence(inputs map toStates)
       s <- get
     } yield (s.coins, s.candies)
   }
