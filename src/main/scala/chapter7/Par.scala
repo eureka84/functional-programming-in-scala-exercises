1091232889bca37f92a5d163d5cc1bf795cb608d
eureka84 <angelosciarra@ymail.com>
2018-11-04 18:11:09 +0100

    first implementation of Par

---
+++ src/main/scala/chapter7/Par.scala
@@ -0,0 +1,90 @@
+package chapter7
+
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
+object Par {
+
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit): A = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+//  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+//    (es: ExecutorService) => {
+//      val af = a(es)
+//      val bf = b(es)
+//      UnitFuture(f(af.get, bf.get))
+//    }
+
+  /* This version respects timeouts. See `Map2Future` below. */
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val (af, bf) = (a(es), b(es))
+      Map2Future(af, bf, f)
+    }
+
+  /*
+    Note: this implementation will not prevent repeated evaluation if multiple threads call `get` in parallel.
+    We could prevent this using synchronization, but it isn't needed for our purposes here (also, repeated evaluation
+    of pure values won't affect results).
+  */
+  case class Map2Future[A, B, C](a: Future[A], b: Future[B], f: (A, B) => C) extends Future[C] {
+
+    @volatile var cache: Option[C] = None
+
+    def isDone: Boolean = cache.isDefined
+
+    def isCancelled: Boolean = a.isCancelled || b.isCancelled
+
+    def cancel(evenIfRunning: Boolean): Boolean =
+      a.cancel(evenIfRunning) || b.cancel(evenIfRunning)
+
+    def get: C = compute(Long.MaxValue)
+
+    def get(timeout: Long, units: TimeUnit): C =
+      compute(TimeUnit.NANOSECONDS.convert(timeout, units))
+
+    private def compute(timeoutInNanos: Long): C = cache match {
+      case Some(c) => c
+      case None =>
+        val start = System.nanoTime
+        val ar = a.get(timeoutInNanos, TimeUnit.NANOSECONDS)
+        val stop = System.nanoTime;
+        val aTime = stop - start
+        val br = b.get(timeoutInNanos - aTime, TimeUnit.NANOSECONDS)
+        val ret = f(ar, br)
+        cache = Some(ret)
+        ret
+    }
+  }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call: A = a(es).get
+    })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+}
+
+object Examples {
+
+  import Par.Par
+
+  def sum(ints: IndexedSeq[Int]): Par[Int] =
+    if (ints.length <= 1)
+      Par.unit(ints.headOption getOrElse 0)
+    else {
+      val (l,r) = ints.splitAt(ints.length/2)
+      Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
+    }
+
+}
